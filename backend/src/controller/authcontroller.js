d78fab16ea365f0089a8407f7d4e7d551ac46bb3
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-01-13 04:30:40 +0500

    commit

--- backend/src/controller/authcontroller.js
+++ backend/src/controller/authcontroller.js
@@ -1,3 +1,4 @@
+import User from "../models/user-model.js";
 export const home = async (req, res) => {
   try {
     res.status(200).send("Home Page");
@@ -7,7 +8,13 @@ export const home = async (req, res) => {
 };
 export const register = async (req, res) => {
   try {
-    res.status(200).send("Registration Page");
+    const { username, email, phone, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+    const newUser = await User.create({ username, email, phone, password });
+    res.status(200).json({ data: newUser });
   } catch (error) {
     console.log("Error in Registration Page", error);
   }
