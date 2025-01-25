a4207bdf166653913aaad5a60bf36604220ca5b3
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-01-26 04:34:56 +0500

    commit

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -22,5 +23,21 @@ const userSchema = new mongoose.Schema({
     default: false,
   },
 });
+
+//secure with bcrypt
+userSchema.pre("save", async function(next){
+const user=this
+if(!user.isModified("password")){
+  next()
+}
+try {
+  const saltRound=await bcrypt.genSalt(10);
+  const hash_password=await bcrypt.hash(user.password, saltRound)
+  user.password=hash_password
+} catch (error) {
+  next(error)
+}
+})
+
 const User = mongoose.model("User", userSchema);
 export default User;
